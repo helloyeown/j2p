be61f8515fcf44675509638bbda97a44ad7085a0
hiYewonMoon1 <toquasimodox@gmail.com>
2023-09-11 20:14:13 +0900

    0711부터

---
+++ src/main/java/com/example/j2p/J2pApplication.java
@@ -0,0 +1,13 @@
+package com.example.j2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2pApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/j2p/J2pApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.j2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
