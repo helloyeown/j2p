2c32aa69aa54a811ff2f2d6689508a8b8dc01196
hiYewonMoon1 <toquasimodox@gmail.com>
2023-09-19 22:05:58 +0900

    0711

--- src/main/java/com/example/j2p/controller/ProductController.java
+++ src/main/java/com/example/j2p/controller/ProductController.java
@@ -2,11 +2,13 @@ package com.example.j2p.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.j2p.dto.PageRequestDTO;
 import com.example.j2p.dto.PageResponseDTO;
+import com.example.j2p.dto.ProductDTO;
 import com.example.j2p.dto.ProductListDTO;
 import com.example.j2p.service.ProductService;
 
@@ -20,10 +22,19 @@ public class ProductController {
     
     private final ProductService service;
 
+    // 목록
     @GetMapping("/list")
     public PageResponseDTO<ProductListDTO> getList(PageRequestDTO requestDTO){
 
         return service.list(requestDTO);
+
+    }
+
+    // 등록
+    @PostMapping("/")
+    public void register(ProductDTO dto){
+
+        service.register(dto);
 
     }
 
