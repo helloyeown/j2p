93b3ffab7e4ab1549715094e12f087ec639d5d6f
hiYewonMoon1 <toquasimodox@gmail.com>
2023-10-03 13:46:23 +0900

    0713

--- src/main/java/com/example/j2p/controller/ProductController.java
+++ src/main/java/com/example/j2p/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,6 +60,16 @@ public class ProductController {
     public ProductDTO readOne(@PathVariable("pno") Long pno){
 
         return service.readOne(pno);
+
+    }
+
+    // 삭제
+    @DeleteMapping("/{pno}")
+    public Map<String, Long> delete(@PathVariable("pno") Long pno){
+
+        service.delete(pno);
+
+        return Map.of("result", pno);
 
     }
 
